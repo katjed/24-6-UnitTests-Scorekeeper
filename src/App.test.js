dc4d184ea6aa118f509688d579f1f8fe88739ae6
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-04-14 22:21:32 +0200

    Add tests

---
+++ src/App.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import App from './App';
+import PlayersList from './components/PlayersList/PlayersList';
+import AddPlayer from './components/AddPlayer/AddPlayer';
+
+it('renders without crashing', () => {
+	shallow(<App />);
+});
+
+it('should update player score', () => {
+	const appComponent = shallow(<App />);
+	const players = [{
+		name: 'Kunegunda',
+		score: 5
+	}]
+
+	appComponent.setState({ players });
+
+	const onScoreUpdate = appComponent.find(PlayersList).prop('onScoreUpdate');
+	onScoreUpdate(0, 5);
+
+	//const playersAfterUpdate = appComponent.state('players');
+	const playersAfterUpdate = appComponent.state().players;
+
+	expect(playersAfterUpdate[0].score).toEqual(10);
+});
+
+
+it('checks player name, new player score and number of players', () => {
+  	const appComponent = shallow(<App />);
+
+  	const onPlayerAdd = appComponent.find(AddPlayer).prop('onPlayerAdd');
+  	onPlayerAdd('Ania');
+
+  	const players = appComponent.state('players');
+
+  	expect(players.length).toEqual(1);
+  	expect(players[0].name).toEqual('Ania');
+  	expect(players[0].score).toEqual(0);
+});
+
